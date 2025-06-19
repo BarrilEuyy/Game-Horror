3e364487042f55e36e21359dc016dcd4b6937ff3
Barril_Dev <barrileuyy@gmail.com>
2025-06-19 19:14:17 +0700

    tambahin light

---
+++ Assets/Scripts/AutoAddMeshCollider.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+
+public class AutoAddMeshCollider : MonoBehaviour
+{
+    void Start()
+    {
+        foreach (var meshFilter in GetComponentsInChildren<MeshFilter>())
+        {
+            var go = meshFilter.gameObject;
+            if (go.GetComponent<Collider>() == null)
+            {
+                go.AddComponent<MeshCollider>();
+            }
+        }
+    }
+}
